89e068dff61988d0390bf2d00f2cf05f3fb4695a
hvullas <ullashv21@gmail.com>
2024-04-03 12:27:42 +0530

    new hello world application

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hvullas/helloWorld-application
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			log.Println("Use http.get")
+		}
+
+		log.Println("Welcome to homepage")
+		outputString, _ := json.Marshal("Welcome to homepage")
+		w.Write(outputString)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			log.Println("Use http.get")
+		}
+
+		log.Println("Welcome to homepage")
+		outputString, _ := json.Marshal("Welcome to homepage")
+		w.Write(outputString)
+	})
+
+	http.HandleFunc("/answer", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			log.Println("Use http.get")
+		}
+
+		log.Println("Who is root?")
+		outputString, _ := json.Marshal("Who is root?")
+		w.Write(outputString)
+	})
+
+	http.ListenAndServe(":80", nil)
+
+}
